e79c142081db3e0396dba654fa1ef5c1ea653527
Zard3017 <ianbradley3017@gmail.com>
2023-05-27 12:29:40 +0300

    added file

---
+++ input.py
@@ -0,0 +1,3 @@
+name = input("What is your name? ")
+fav_colour = input("What is your favorite color?")
+print(name + "likes " + fav_colour)
---
+++ typeconversion.py
@@ -0,0 +1,13 @@
+#birth_year = input("Birth year: ")
+#print(type(birth_year))
+
+#age = 2023 - int(birth_year)
+
+weight_lbs = input("Weight (lbs):")
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+
+
+#print(type(age))
+
+#print(age)
